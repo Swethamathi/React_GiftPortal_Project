2af5de81300c6e84f8c70433895871c888c2dbce
Swethamathi <125295769+Swethamathi@users.noreply.github.com>
2024-04-13 10:06:11 +0530

    Backend/Review-1

---
+++ Backend[Review-1]/Backend_GiftPortal/Backend/Gift/gift/src/main/java/swethamathi/gift/GiftApplication.java
@@ -0,0 +1,14 @@
+package swethamathi.gift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class GiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftApplication.class, args);
+    }
+
+}
